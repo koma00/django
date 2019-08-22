c253ff540bf175e63d0b19fd6aae6b4e14731f66
koma00 <koma196604@gmail.com>
2019-08-22 21:21:34 +0300

    Создаем контроллер и маршрут к нему

--- samplesite/samplesite/urls.py
+++ samplesite/samplesite/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from bboard.views import index
+
 urlpatterns = [
+    url(r'^bboard/', index),
     url(r'^admin/', admin.site.urls),
 ]
