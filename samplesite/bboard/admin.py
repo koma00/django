051350daea6decdd95951dd93ac674750c9f6815
koma00 <koma196604@gmail.com>
2019-09-05 20:52:47 +0300

    Added a model RuЬric and  model link

--- samplesite/bboard/admin.py
+++ samplesite/bboard/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Bb
+from .models import Rubric
 
 class BbAdmin(admin.ModelAdmin):
     list_display = ('title', 'content', 'price', 'published')
@@ -10,3 +11,4 @@ class BbAdmin(admin.ModelAdmin):
     search_fields = ('title', 'content', )
 
 admin.site.register(Bb, BbAdmin)
+admin.site.register(Rubric)
